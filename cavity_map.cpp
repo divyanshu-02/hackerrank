b6e705f53947a3d9868d800f36bd887e20b3662b
divyanshu-02 <66309422+divyanshu-02@users.noreply.github.com>
2020-07-19 00:04:24 +0530

    Create cavity_map.cpp

---
+++ cavity_map.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    char a[n][n];
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cin>>a[i][j];
+        }
+    }
+    for(i=1;i<n-1;i++)
+    {
+        for(j=1;j<n-1;j++)
+        {
+            if(a[i-1][j]<a[i][j]&&a[i+1][j]<a[i][j]&&a[i][j-1]<a[i][j]
+            &&a[i][j+1]<a[i][j])
+            {
+                a[i][j]='X';
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            cout<<a[i][j];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
